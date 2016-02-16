b79d93437956200010b763793ae3f8a7370207fe
Matt DePorter <nfaction@gmail.com>
2016-02-16 14:27:36 -0700

    Add hardware time sync and debug to dhcp HN script 	modified:   roles/atmo-dhcp/files/hostname-exit-hook.sh

--- ansible/roles/atmo-dhcp/files/hostname-exit-hook.sh
+++ ansible/roles/atmo-dhcp/files/hostname-exit-hook.sh
@@ -2,6 +2,12 @@
 
 LOG=/var/log/atmo/dhcp_hostname.log
 
+# Sync OS time using the host's hardware time
+echo $(date +"%m%d%y %H:%M:%S") "Syncing system time with hardware time" >> $LOG
+echo "     DEBUG: Date BEFORE sync:" $(date) >> $LOG
+hwclock -s
+echo "     DEBUG: Date AFTER sync:" $(date)  "hwclock exit status: $?" >> $LOG
+
 # Function: get_hostname()
 # Description: Gets the hostname, depending on the distro
 get_hostname() {
